9e4591ac34e82c10eb20a5daea3706f31f0226b6
Leonardo <leogpassarini@gmail.com>
2026-02-18 19:18:17 -0300

    0822

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import { UserContext } from '../../Contexts/UserContext';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
-  return <div></div>;
+const PhotoComments = (props) => {
+  const { loggedIn } = React.useContext(UserContext);
+  const [comments, setComments] = React.useState(() => props.comments);
+
+  return (
+    <div>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {loggedIn && (
+        <PhotoCommentsForm id={props.id} setComments={setComments} />
+      )}
+    </div>
+  );
 };
 
 export default PhotoComments;
---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { COMMENT_POST } from '../../API';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import Enviar from '../../Assets/enviar.svg?react';
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const token = window.localStorage.getItem('token');
+    const { url, options } = COMMENT_POST(id, { comment }, token);
+    const { response, json } = await request(url, options);
+
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
