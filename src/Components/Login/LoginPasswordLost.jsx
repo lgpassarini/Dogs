31eeea2d140dd1504589c68f61ebded19aa2fd57
Leonardo <leogpassarini@gmail.com>
2026-02-24 18:31:42 -0300

    0832

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import Error from '../Helper/Error';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../api';
 
 const LoginPasswordLost = () => {
-  return <div></div>;
+  const login = useForm(null, true);
+  const { data, error, loading, request } = useFetch();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!login.validate()) return;
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: window.location.href.replace('perdeu', 'resetar'),
+    });
+    request(url, options);
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
