a41bd47b6b3c0cbcf4a4eb4f2c81e9ba170cc5a8
Leonardo <leogpassarini@gmail.com>
2026-03-02 18:24:11 -0300

    0835

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../API';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Stats
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem('token');
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
---
+++ src/Components/User/UserStatsGraphs.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styles from './UserStatsGraphs.module.css';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
+
+const UserStatsGraphs = ({ data }) => {
+  const [graph, setGraph] = React.useState(null);
+  const [total, setTotal] = React.useState(0);
+
+  React.useEffect(() => {
+    if (data) {
+      setGraph(
+        data.map(({ title, acessos }) => {
+          return {
+            x: title,
+            y: Number(acessos),
+          };
+        }),
+      );
+      setTotal(
+        data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b, 0),
+      );
+    }
+  }, [data]);
+  return (
+    <section className={`${styles.graph} animeLeft`}>
+      <div className={`${styles.total} ${styles.graphItem}`}>
+        <p>Acessos: {total}</p>
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryPie
+          data={graph}
+          innerRadius={50}
+          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+          style={{
+            data: {
+              fillOpacity: 0.9,
+              stroke: '#fff',
+              strokeWidth: 2,
+            },
+            labels: {
+              fontSize: 14,
+              fill: '#333',
+            },
+          }}
+        />
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryChart>
+          <VictoryBar alignment="start" data={graph}></VictoryBar>
+        </VictoryChart>
+      </div>
+    </section>
+  );
+};
+
+export default UserStatsGraphs;
